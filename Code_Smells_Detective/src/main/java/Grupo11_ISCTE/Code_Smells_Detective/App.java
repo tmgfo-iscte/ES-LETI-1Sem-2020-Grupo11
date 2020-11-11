a85e6ebe845294db8b966a47956c7467b267e2ba
m7911-iscte <m7911@iscte-iul.pt>
2020-11-11 12:02:14 +0000

    Início do projeto

---
+++ Code_Smells_Detective/src/main/java/Grupo11_ISCTE/Code_Smells_Detective/App.java
@@ -0,0 +1,14 @@
+package Grupo11_ISCTE.Code_Smells_Detective;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+ 
+    	System.out.println( "Hello World!" );
+    }
+}
